14c52051a9e65451671730e826681b43b95d1168
yang wang <xyyr190304@gmail.com>
2021-12-29 12:20:21 +0100

    Still missing subscription control is not implemented

--- src/main/java/com/example/j2ee_project/Model/RelationEntity.java
+++ src/main/java/com/example/j2ee_project/Model/RelationEntity.java
@@ -1,15 +1,14 @@
 package com.example.j2ee_project.Model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Objects;
 
 @Entity
 @Table(name = "relation", schema = "j2eeproject", catalog = "")
 public class RelationEntity {
     private int idRelation;
+    private int idFollowTo;
+    private int idFollowFrom;
 
     @Id
     @Column(name = "idRelation")
@@ -32,5 +31,34 @@ public class RelationEntity {
     @Override
     public int hashCode() {
         return Objects.hash(idRelation);
+    }
+
+    @Basic
+    @Column(name = "idFollowTo")
+    public int getIdFollowTo() {
+        return idFollowTo;
+    }
+
+    public void setIdFollowTo(int idFollowTo) {
+        this.idFollowTo = idFollowTo;
+    }
+
+    @Basic
+    @Column(name = "idFollowFrom")
+    public int getIdFollowFrom() {
+        return idFollowFrom;
+    }
+
+    public void setIdFollowFrom(int idFollowFrom) {
+        this.idFollowFrom = idFollowFrom;
+    }
+
+    @Override
+    public String toString() {
+        return "RelationEntity{" +
+                "idRelation=" + idRelation +
+                ", idFollowTo=" + idFollowTo +
+                ", idFollowFrom=" + idFollowFrom +
+                '}';
     }
 }
