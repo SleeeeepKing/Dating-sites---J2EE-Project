6de54f3203b9903723a22133f5c6cccc9114eba0
Wenlei LUO <luowenlei724@gmail.com>
2021-12-30 03:38:35 +0100

    Dao

--- src/main/java/com/example/j2ee_project/DAO/main.java
+++ src/main/java/com/example/j2ee_project/DAO/main.java
@@ -1,11 +1,11 @@
 package com.example.j2ee_project.DAO;
 
-import Model.Condition;
+import com.example.j2ee_project.Model.Condition;
 
 public class main {
     public static void main(String[] args) {
         UserDao userDao = new UserDao();
-        System.out.println(userDao.searchById(3));
+//        System.out.println(userDao.searchById(3));
         RelationDao relationDao=new RelationDao();
         ProfileDao profileDao=new ProfileDao();
 //        System.out.println(profileDao.searchById(1));
@@ -15,7 +15,7 @@ public class main {
         condition.setGender("F");
         condition.setLocate("Paris");
         condition.setStatus("Single");
-//        System.out.println(profileDao.searchByConditions(condition));
+        System.out.println(profileDao.searchByConditions(condition));
 //        System.out.println(condition.getGender());
     }
 }
